04c4b4b4a08f7ffb58ea70dd37ac6221ad7dfd71
mattlanes <mattlanes82@gmail.com>
2022-02-13 15:25:21 -0600

    Finalizing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
+window.onscroll = function() {scrollFunction()};
 
-// When the user clicks on the button, scroll to the top of the document
+function scrollFunction() {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0;
 }
